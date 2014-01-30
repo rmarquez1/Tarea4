7ce06fa8417646736486fb3693d263bf3cfa3775
Ramon <albertocomp10@gmail.com>
2014-01-30 17:14:47 -0430

    Información para el test.py para el clei

--- CLEI/CLEI/apps/clei/tests.py
+++ CLEI/CLEI/apps/clei/tests.py
@@ -45,41 +45,60 @@ class CleiTest(TestCase):
         # Creando Articulos
         # --------------------------------------------------------------------- 
         articulo1 = Articulo.objects.create(id_articulo=1, titulo='Ingenieria de software', 
-                                            resumen='Resumen',
+                                            resumen='Resumen', aceptado=False,
+                                            aceptado_especial=False, 
+                                            rechazado_falta_cupo=False,
+                                            rechazado_por_promedio=False,
                                             texto='Texto', p1='ingenieria', 
                                             p2='software')
         articulo1.autores = [autor1]
 
 
         articulo2 = Articulo.objects.create(id_articulo=2, titulo='Sistema de Informacion', 
-                                            resumen='Resumen',
+                                            resumen='Resumen', aceptado=False,
+                                            aceptado_especial=False, 
+                                            rechazado_falta_cupo=False,
+                                            rechazado_por_promedio=False,
                                             texto='Texto', p1='sistema', 
                                             p2='informacion', p3='stakeholders')
         articulo2.autores = [autor2]
 
 
         articulo3 = Articulo.objects.create(id_articulo=3, titulo='Software', 
-                                            resumen='Resumen', texto='Texto', 
-                                            p1='')
+                                            resumen='Resumen', aceptado=False,
+                                            aceptado_especial=False, 
+                                            rechazado_falta_cupo=False,
+                                            rechazado_por_promedio=False, 
+                                            texto='Texto', p1='')
         articulo3.autores = [autor3]
 
 
 
         articulo4 = Articulo.objects.create(id_articulo=4, titulo='Embarazo', 
-                                            resumen='Resumen', texto='Texto', 
+                                            resumen='Resumen', aceptado=False,
+                                            aceptado_especial=False, 
+                                            rechazado_falta_cupo=False,
+                                            rechazado_por_promedio=False,
+                                            texto='Texto', 
                                             p1='embarazo', p2='mujer')
         articulo4.autores = [autor4]
 
 
 
         articulo5 = Articulo.objects.create(id_articulo=5, titulo='Lenguaje de programacion', 
-                                            resumen='Resumen', texto='Texto', 
+                                            resumen='Resumen', aceptado=False,
+                                            aceptado_especial=False, 
+                                            rechazado_falta_cupo=False,
+                                            rechazado_por_promedio=False, texto='Texto', 
                                             p1='lenguaje', p2='programacion')
         articulo5.autores = [autor5]
 
 
         articulo6 = Articulo.objects.create(id_articulo=6, titulo='Base de datos', 
-                                            resumen='Resumen',
+                                            resumen='Resumen', aceptado=False,
+                                            aceptado_especial=False, 
+                                            rechazado_falta_cupo=False,
+                                            rechazado_por_promedio=False,
                                             texto='Texto', p1='base de datos')
 
         articulo6.autores= [autor1]
@@ -88,7 +107,11 @@ class CleiTest(TestCase):
 
         articulo7 = Articulo.objects.create(id_articulo=7, 
                                             titulo='Software Libre vs Software Propietario', 
-                                            resumen='Resumen', texto='Texto', 
+                                            resumen='Resumen', aceptado=False,
+                                            aceptado_especial=False, 
+                                            rechazado_falta_cupo=False,
+                                            rechazado_por_promedio=False,
+                                            texto='Texto', 
                                             p1='software libre', 
                                             p2='software propietario')
         articulo7.autores = [autor6]
@@ -138,10 +161,99 @@ class CleiTest(TestCase):
 
     # Test de listar los paises participantes en la conferencia
     def test_paises_conferencia(self):
-
         clei = CLEI()
         clei.crear_articulos()
         clei.crear_aceptables()
 
         lista_paises = clei.paises_conferencia()
         self.assertEquals('Italia', lista_paises[0])
+
+
+    # Test que lista los articulos de cada pais
+    def test_listar_articulos_pais(self):
+        clei = CLEI()
+        clei.crear_articulos()
+        clei.crear_aceptables()
+
+        lista_paises = clei.listar_articulos_por_pais(2)
+        self.assertEquals(len(lista_paises[0][1]), 2)
+
+
+    # Test de cantidad minima de articulos
+    def test_cantidad_min_articulos(self):
+        clei = CLEI()
+        clei.crear_articulos()
+        clei.crear_aceptables()
+
+        lista_min = clei.cantidad_min_articulos(2)
+        self.assertEquals(len(lista_min[0][1]), 2)
+        self.assertEquals(len(lista_min[1][1]), 2)
+
+
+    # Test de agregar los aceptados
+    def test_agregar_aceptados(self):
+        clei = CLEI()
+        clei.crear_articulos()
+        clei.crear_aceptables()
+
+        clei.set_num_articulos(4)
+        num_articulos = clei.agregar_aceptados(2)
+        self.assertEquals(0, num_articulos)
+
+
+        clei.set_num_articulos(5)
+        num_articulos = clei.agregar_aceptados(2)
+        self.assertEquals(1, num_articulos)
+
+
+    # Test de seleccion por desempate
+    def test_seleccion_desempate(self):
+        clei = CLEI()
+        clei.crear_articulos()
+        clei.crear_aceptables()
+
+        clei.set_num_articulos(2)
+        promedios = clei.listar_promedios(clei.get_aceptables())
+        limite = clei.crear_aceptados_empatados(promedios, clei.get_aceptables())
+        self.assertEquals(7, clei.get_aceptados()[0])
+        self.assertEquals(3, clei.get_empatados()[0])
+
+
+        clei = CLEI()
+        clei.crear_articulos()
+        clei.crear_aceptables()
+        clei.set_num_articulos(4)
+        promedios = clei.listar_promedios(clei.get_aceptables())
+        limite = clei.crear_aceptados_empatados(promedios, clei.get_aceptables())
+        self.assertEquals(7, clei.get_aceptados()[0])
+        self.assertEquals(1, len(clei.get_empatados()))
+
+
+    # Test de seleccion por pais
+    def test_seleccion_pais(self):
+        clei = CLEI()
+        clei.crear_articulos()
+        clei.crear_aceptables()
+
+        clei.set_num_articulos(4)
+        num_articulos = clei.agregar_aceptados(2)
+        clei.seleccionar_por_pais(2)
+        self.assertEquals(0, clei.get_num_articulos())
+
+        clei.set_num_articulos(5)
+        num_articulos = clei.agregar_aceptados(2)
+        clei.seleccionar_por_pais(2)
+        self.assertEquals(1, clei.get_num_articulos())
+
+
+    # Test de estados de seleccion
+    def test_estados_seleccion(self):
+        clei = CLEI()
+        clei.crear_articulos()
+        clei.crear_aceptables()
+        clei.set_num_articulos(4)
+        promedios = clei.listar_promedios(clei.get_aceptables())
+        limite = clei.crear_aceptados_empatados(promedios, clei.get_aceptables())
+        articulo = Articulo.objects.filter(aceptado=True)
+        self.assertTrue(articulo[0].aceptado, "El articulo debe ser aceptado")
+        
